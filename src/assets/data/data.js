556358ab9965ceddded5c59dcde0db1994b0696a
Juana-89 <juana.lc@hotmail.es>
2023-12-12 18:33:06 -0500

    projects finished

---
+++ src/assets/data/data.js
@@ -0,0 +1,111 @@
+export const projects = [
+    {
+        id: 0, 
+        tech: 'HTML', 
+        name: '404 Not Found',
+        hashtag:"#HTML",
+        description: 'This challenge is suitable for you if you just started with coding. The challenge will test your basic CSS and HTML skills.', 
+        repo: 'https://github.com/Juana-89/404-not-found', 
+        prd: 'https://404-not-found-juana-89.vercel.app/', 
+        img:'https://i.ibb.co/ZT0b3mT/Captura-de-pantalla-2023-12-11-012630.png'
+    },
+    {
+        id: 1, 
+        tech: 'HTML', 
+        name: 'My team page', 
+        hashtag:"#HTML",
+        description: 'This challenge will test your basic CSS skills and get it to next level. You will work with basic HTML element to make a simple but yet elegant team page.', 
+        repo: 'https://github.com/Juana-89/my-team-page-master', 
+        prd: 'https://juana-89.github.io/my-team-page-master/', 
+        img:'https://i.ibb.co/3RszRLb/Captura-de-pantalla-2023-12-11-013119.png'
+    },
+    {
+        id: 2, 
+        tech: 'Javascript', 
+        hashtag:"#Javascript",
+        name: 'Interior Consultant', 
+        description: 'Your challenge is to create a simple interior consultant homepage. This challenge will test your skill in styling navigation and positioning.', 
+        repo: 'https://juana-89.github.io/my-team-page-master/', 
+        prd: 'https://mellow-kataifi-038f1c.netlify.app/', 
+        img:'https://i.ibb.co/9ZBMbbC/Captura-de-pantalla-2023-12-11-013303.png'
+    },
+    {
+        id: 3, 
+        tech: 'HTML', 
+        name: 'Recipe page', 
+        hashtag:"#HTML",
+        description: 'Everyone loves food, especially cheesecake. In this challenge, you will work with a recipe of your choice or given one.', 
+        repo: 'https://github.com/Juana-89/recipe-page-master', 
+        prd: 'https://recipe-page-master-three.vercel.app/', 
+        img:'https://i.ibb.co/cv9pQfP/Captura-de-pantalla-2023-12-11-013427.png'
+    },
+    {
+        id: 4, 
+        tech: 'CSS', 
+        name: 'My Gallery',
+        hashtag:"#CSS",
+        description: 'Inspired by instagram, in this challenge you will work with a similar design to instagram profile page.', 
+        repo: 'https://github.com/Juana-89/recipe-page-master', 
+        prd: 'https://recipe-page-master-three.vercel.app/', 
+        img:'https://i.ibb.co/rfDDzZm/Captura-de-pantalla-2023-12-11-013542.png'
+    },
+    {
+        id: 5, 
+        tech: 'CSS', 
+        name: 'Checkout Page', 
+        hashtag:"#CSS",
+        description: 'In this challenge, you will work with the form in a modern and minimal design. The challenge will bring your HTML and CSS to a new level. ',
+        repo: 'https://github.com/Juana-89/checkout-page-master', 
+        prd: 'https://checkout-page-master-three.vercel.app/', 
+        img:'https://i.ibb.co/rfDDzZm/Captura-de-pantalla-2023-12-11-013542.png'
+    },
+    {
+        id: 6, 
+        tech: 'CSS', 
+        hashtag:"#CSS",
+        name: 'Edie  homepage',
+        description: 'Homepage is a popular task that a developer has to do. In this challenge, you will work with Edie - a small team providing web services.', 
+        repo: 'https://github.com/Juana-89/edie-homepage-master', 
+        prd: 'https://edie-homepage-master-eta.vercel.app/', 
+        img:'https://i.ibb.co/TLK0CWV/Captura-de-pantalla-2023-12-11-013902.png'
+    },
+    {
+        id: 7, 
+        tech: 'Javascript', 
+        name: 'Info plants', 
+        description: 'Homepage is a popular task that a developer has to do. In this challenge, you will work with Edie - a small team providing web services.', 
+        repo: 'https://github.com/Juana-89/info-plants', 
+        prd: 'https://info-plants-8t1z.vercel.app/', 
+        img:'https://i.ibb.co/rfDDzZm/Captura-de-pantalla-2023-12-11-013542.png'
+    },
+    {
+        id: 8, 
+        tech: 'Javascript', 
+        name: 'Data Lovers',
+        hashtag:"#javascript", 
+        description: 'The website developed is aimed at providing information to all people interested in learning about the animation work of Studio Ghibli.', 
+        repo: 'https://github.com/Juana-89/LIM017-data-lovers', 
+        prd: 'https://juana-89.github.io/LIM017-data-lovers/', 
+        img:'https://i.ibb.co/3RszRLb/Captura-de-pantalla-2023-12-11-013119.png'
+    },
+    {
+        id: 9, 
+        tech: 'React', 
+        name: 'Calculator', 
+        hashtag:"#React",
+        description: 'It is a responsive application of a simple counter made in React practicing the basic fundamentals in this popular framework.', 
+        repo: 'https://github.com/Juana-89/Counter-React', 
+        prd: 'https://juana-89.github.io/LIM017-data-lovers/', 
+        img:'https://i.ibb.co/ZT0b3mT/Captura-de-pantalla-2023-12-11-012630.png'
+    },
+    {
+        id: 10, 
+        tech: 'React', 
+        hashtag:"#React",
+        name: 'Burger queen', 
+        description: 'Interface made in React connected to Firestore DB that allows you to take note of orders and later send them to the kitchen for preparation.', 
+        repo: 'https://github.com/Juana-89/LIM017-burger-queen', 
+        prd: 'http://lim-017-burger-queen-omega.vercel.app/',
+        img:'https://i.ibb.co/rMNshy8/burger-queen.png'
+    }
+]
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,11 +1,94 @@
-import React from 'react'
-import { Card, Flex, Typography, Progress, Image } from 'antd';
+import { useState, useEffect } from 'react'
+import { Card, Flex, Typography, Button, Image } from 'antd';
+import {projects} from '../assets/data/data.js'
 import Adidas from '../assets/Adidas_Logo.svg.png';
 import HyM from '../assets/H&M-Logo.svg.png';
 
 const FilterProjects = () => {
+  const cardStylesProjects = {
+    width: 1048,
+    height: 115,
+    margin: 'auto'
+  };
+
+  const buttonStyles = {
+    backgroundColor: '#fff',
+    color: '#000',
+    border: '1px solid black',
+    marginRight: 20,
+    transition: 'background-color 0.3s ease', 
+    ':hover': {
+      backgroundColor: '#1890ff',
+      color: '#fff',
+    },
+  }
+
+  const cardStylesSelectedProjects = {
+    maxWidth: 329,
+    width:'100%',
+    height: 568,
+    marginRight: 20
+  };
+
+  const [showTechs, setShowTechs] = useState([]);
+  const [filteredTechNoRepeat, setFilteredTechNoRepeat ] = useState([])
+  const [selectProject, setSelectProject] = useState(null);
+
+  useEffect(() => {
+    setShowTechs(projects);
+    setFilteredTechNoRepeat([...new Set(projects.map((p) => p.tech))]);
+  }, []);
+
+  const showProjects = (tech) => {
+     setSelectProject(tech);
+  }
+
   return (
-    <div>Filter</div>
+    <Flex vertical style={{marginTop: 20}}>
+      <Card
+      hoverable
+      style={cardStylesProjects}
+      >
+        <Typography.Title level={4} style= {{marginTop:5 }}>
+          Projects
+        </Typography.Title>
+        <Flex horizontal style={{marginBottom:50}}>
+        {filteredTechNoRepeat.map((project, index) => (
+          <div key={index}>
+            <Button 
+              type="primary" 
+              key={index} 
+              onClick={()=>showProjects(project)}
+              style={buttonStyles}>
+              {project}
+            </Button>
+          </div>
+        ))}
+        </Flex>
+        {selectProject && (
+          <Flex style={{marginTop: 20, marginLeft: -25}} gap='middle'>
+            {showTechs
+              .filter((project) => project.tech === selectProject)
+              .map((project, index) => (
+                <Card key={index} style={cardStylesSelectedProjects}>
+                  <img src={project.img} style={{width: '292px', height: '170px'}}/>
+                  <p style={{fontSize: '16px'}}>{project.hashtag}</p>
+                  <Typography.Title level={5} style={{marginTop: 8}}>{project.name}</Typography.Title>
+                  <p>{project.description}</p>
+                  <Flex gap='large' style={{marginTop:30}}>
+                    <a href= {project.repo} target="_blank">
+                      <Button type="primary" size='large'>Demo</Button>
+                    </a>
+                    <a href= {project.prd} target="_blank">
+                      <Button type="primary" size='large' ghost>Code</Button>
+                    </a>
+                  </Flex>
+                </Card>
+              ))}
+          </Flex>
+        )}
+      </Card>
+    </Flex>
   )
 }
 
